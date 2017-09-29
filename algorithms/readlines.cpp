4b55a907d8d17b475ab3b90c24cb571a99c9f165
vinaykumar2491 <vinaykumar2491@gmail.com>
2017-09-30 00:52:25 +0530

    _v final readlines

--- algorithms/readlines.cpp
+++ algorithms/readlines.cpp
@@ -1,26 +1,32 @@
 /*
-
-
 */
 
 #include <iostream>
 #include <string>
+#include <vector>
 
-using std::string;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+using std::vector;
 
 int main(){
 
 	// reading per word. Starting and ending whitespaces are ignored
-	string s1, s2;					// Empty strings
-	cin >> s1 >> s2;				// reading first two words separated by 'whitespace' character in a single line
-	cout << s1 << s2 << endl;		// printing the read words
-	
+//	string s1, s2;					// Empty strings
+//	cin >> s1 >> s2;				// reading first two words separated by 'whitespace' character in a single line
+//	cout << s1 << s2 << endl;		// printing the read words
+//	
 	// reading a complete line. Starting and ending whitespaces are NOT ignored
 	string m_line;
 	getline(cin, m_line);
+	cout << m_line << endl;
+	
+	//Capitalize the line using iterator
+	for(auto iter= m_line.begin(); iter != m_line.end() && !isspace(*iter); ++iter){
+		*iter = toupper(*iter);
+	}
 	cout << m_line << endl;
 	
 	//----------------------------------------------------------------------------
@@ -28,7 +34,7 @@ int main(){
 	string result;						// holds the hexif'ed string
 	string::size_type n;				// Holds the numbers from input
 	
-	cout << "Enter a series of numbers b/w 0 and 15"
+	cout << "Enter a series of numbers b/w 0 and 15 "
 		 << "separated by SPACES. Hit ENTER when done: "
 		 << endl;
 	while (cin >> n){					// checks if the input is empty or reached EOF
@@ -38,9 +44,10 @@ int main(){
 	}
 	
 	cout << result << endl;	
-
-	return 0;						// SUCCESS
-		
+	
+	//----------------------------------------------------------------------------
+	
+	return 0;						// SUCCESS	
 }
 
 
