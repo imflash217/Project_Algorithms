028f721285181741b233b2dd58cf35d9eda9448b
vinaykumar2491 <vinaykumar2491@gmail.com>
2017-11-06 19:40:19 +0530

    class scopes, return types, friend

--- algorithms/Screen.h
+++ algorithms/Screen.h
@@ -5,12 +5,30 @@
 
 #include <iostream>
 #include <string>
-#include <cstddef>
+#include <cstddef>		// to use size_t
+#include <vector>
+
+class Window_mgr{
+
+public:
+	// location ID for each screen on the Window
+	using ScreenIndex = std::vector<Screen>::size_type;		// local type name
+	// reset the Screen at the given position to all blanks
+	void clear(ScreenIndex);	// function declaration
+	ScreenIndex addScreen(const Screen&);
+
+private:
+	std::vector<Screen> screens{Screen(20, 17, "Rustom Potter")};
+
+};
 
 class Screen{
 
 // Window_mgr is a 'friend' of Screen; so Window_mgr can access 'private' members of Screen
-friend class Window_mgr;
+// friend class Window_mgr;
+
+// declaring 'clear' as a friend of 'Screen'
+friend void Window_mgr::clear(ScreenIndex);
 
 public: 
 	typedef std::string::size_type pos;		// a "type member"
@@ -29,10 +47,24 @@ private:
 	std::string contents;
 
 	mutable size_t access_ctr;		// may be changed even in a 'const' object
+};
+
+// member function definition
+void Window_mgr::clear(ScreenIndex i){
+	// s is a reference to the screen we have to clear
+	Screen &s = screens[i];
+	// reset the contents of that screen to all blanks
+	s.contents = std::string(s.width * s.height, ' ');	
+}
+
+// return type is sceen before we are in the scope of Window_mgr
+Window_mgr::ScreenIndex Window_mgr::addScreen(const Screen &s){
+	screens.push_back(s);
+	return screens.size() - 1;
 }
 
 // declared as 'inline' in the function DECLARATION inside the class
-char get(pos r, pos c) const {
+char Screen::get(pos r, pos c) const {
 	pos row = r * width;			// compute new row 
 	return contents[row + c];		// return character at a given column
 }
